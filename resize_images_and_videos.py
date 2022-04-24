8742234068c81368c429f90e08478f12486bb006
harshraj2717 <harshraj2717@gmail.com>
2022-04-24 20:34:25 +0530

    Completed resizing_images_and_videos.py

--- resize_images_and_videos.py
+++ resize_images_and_videos.py
@@ -1,21 +1,27 @@
 import cv2 as cv
 
 
+def resize_frame(frame, scale):
+    height = int(frame.shape[0] * scale)
+    width = int(frame.shape[1] * scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions)
+
 def image_read():
     img_location = input("Enter image location : ")
     img = cv.imread(img_location)
 
-    # not needed just showing what imread does...
-    # print(img_matrix)
+    scale = float(input("Enter scale : "))
+    resized_img = resize_frame(img, scale)
 
-    cv.imshow("image", img)
-
+    cv.imshow("image", resized_img)
     cv.waitKey(0)
 
 
 def video_read():
     vid_location = input("Enter video location : ")
     vid = cv.VideoCapture(vid_location)
+    scale = float(input("Enter scale : "))
 
     # not needed just showing what imread does...
     # print(vid)
@@ -23,7 +29,8 @@ def video_read():
     isTrue = True
     isTrue, frame = vid.read()
     while isTrue:
-        cv.imshow("video", frame)
+        resized_frame = resize_frame(frame, scale)
+        cv.imshow("video", resized_frame)
 
         if cv.waitKey(20) and 0xFF == ord("q"):
             break
@@ -32,6 +39,7 @@ def video_read():
 
     vid.release()
     cv.destroyAllWindows()
+
 
 
 def main():
